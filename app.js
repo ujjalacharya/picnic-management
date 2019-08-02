56fa2706b5d998cc8cd91288a8be46ad3fd43aa1
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-08-02 19:27:26 +0545

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dbConnection = require("./helpers/dbConnection");
 const expressValidator = require("express-validator");
 const express = require("express");
 require("express-async-errors");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
@@ -9,6 +10,7 @@ const app = express();
 dbConnection();
 
 //Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(expressValidator());
 
@@ -19,12 +21,14 @@ app.use("/api/student", require("./routes/student"));
 
 // Error handling middleware
 app.use(function(err, req, res, next) {
- console.log(err);
- res.status(500).json({message: err._message || err.errmsg || "Something went wrong"});
+  console.log(err);
+  res
+    .status(500)
+    .json({ message: err._message || err.errmsg || "Something went wrong" });
 });
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
